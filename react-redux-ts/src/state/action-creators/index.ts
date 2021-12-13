32dd3c032c0016f3b7bd6e7556520bede691cf20
SumanDontha16 <51446173+SumanDontha16@users.noreply.github.com>
2021-12-13 20:22:50 +0000

    Created store and action creators.

---
+++ react-redux-ts/src/state/action-creators/index.ts
@@ -0,0 +1,32 @@
+import { ActionType } from '../action-types'
+import { Dispatch } from 'redux'
+import { Action } from '../actions/index'
+
+// DepositAction
+export const depositMoney = (amount: number) => {
+    return (dispatch: Dispatch<Action>) => {
+        dispatch({
+            type: ActionType.DEPOSIT,
+            payload: amount
+        })
+    }
+}
+
+// WithdrawAction
+export const withdrawMoney = (amount: number) => {
+    return (dispatch: Dispatch<Action>) => {
+        dispatch({
+            type: ActionType.WITHDRAW,
+            payload: amount
+        })
+    }
+}
+
+// BankruptAction
+export const bankruptMoney = (amount: number) => {
+    return (dispatch: Dispatch<Action>) => {
+        dispatch({
+            type: ActionType.BANKRUPT,
+        })
+    }
+}
